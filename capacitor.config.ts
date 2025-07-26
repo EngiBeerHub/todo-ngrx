cd2749332622c9f336323af2f69c036d829b5f68
EngiBeerHub <engibeer0422@gmail.com>
2025-07-26 11:39:07 +0900

    Synced Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'todo.ngrx',
-  appName: 'todo-ngrx',
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app-base',
   webDir: 'www'
 };
 
